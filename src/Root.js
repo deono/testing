09ee3b23cfd4953c0cd7fb9035b70db8df3d8109
Deon Odendaal <deon.odendaal@protonmail.com>
2020-01-21 18:43:00 +0000

    CommentList tests

--- src/Root.js
+++ src/Root.js
@@ -3,8 +3,9 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "reducers";
 
-export default props => {
+export default ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, {})}>{props.children}</Provider>
+    // if the initialState is not passed in as props, it will default to an empty object
+    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
   );
 };
