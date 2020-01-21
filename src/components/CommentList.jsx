d2b0c72ccfa8df22ca0593c23a914d1e4cb9cb2c
Deon Odendaal <deon.odendaal@protonmail.com>
2020-01-21 12:42:38 +0000

    CommentList component

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,3 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default () => <div>Comment List</div>;
+export class CommentList extends Component {
+  renderComments() {
+    const { comments } = this.props;
+    return comments.map((comment, index) => {
+      return <li key={index}>{comment}</li>;
+    });
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h4>Comment List</h4>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    comments: state.comments
+  };
+};
+
+export default connect(mapStateToProps)(CommentList);
