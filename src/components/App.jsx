59164e5d9137260da6216806f3fee3b547a6d340
Deon Odendaal <deon.odendaal@protonmail.com>
2020-01-22 10:37:54 +0000

    higher order component for auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
 import CommentList from "components/CommentList";
 import CommentBox from "components/CommentBox";
+import * as actions from "actions";
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route exact path="/" component={CommentList} />
-    </div>
-  );
-};
+class App extends Component {
+  // produce the signIn button
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+      );
+    }
+  }
+  // produce the JSX for the nav header
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Add Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route exact path="/" component={CommentList} />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, actions)(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import commentsReducers from "reducers/commentsReducer";
+import authReducer from "reducers/authReducer";
 
 export default combineReducers({
-  comments: commentsReducers
+  comments: commentsReducers,
+  auth: authReducer
 });
